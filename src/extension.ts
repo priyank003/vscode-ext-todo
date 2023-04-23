f123b3337c8a460e44692fd597e77a061d77296d
Priyank <priyankcrjr7@gmail.com>
2023-04-24 01:59:42 +0530

    inital commit

---
+++ src/extension.ts
@@ -0,0 +1,29 @@
+import * as vscode from "vscode";
+import { HelloWorldPanel } from "./HelloWorldPanel";
+
+export function activate(context: vscode.ExtensionContext) {
+  console.log('Congratulations, your extension "vstodo" is now active!');
+
+  context.subscriptions.push(
+    vscode.commands.registerCommand("vstodo.helloWorld", () => {
+      HelloWorldPanel.createOrShow(context.extensionUri);
+    })
+  );
+
+  context.subscriptions.push(
+    vscode.commands.registerCommand("vstodo.askQuestion", async () => {
+      const answer = await vscode.window.showInformationMessage(
+        "How was your day?",
+        "good",
+        "bad"
+      );
+      if (answer === "bad") {
+        vscode.window.showInformationMessage("Sorry to hear that");
+      } else {
+        console.log(answer);
+      }
+    })
+  );
+}
+
+export function deactivate() {}
